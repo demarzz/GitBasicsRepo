a39d3a74f4bcd498726ab6661a3c6e0bbf678b45
Aditya Gautam <adityagautam2011@gmail.com>
2020-09-05 10:57:41 +0530

    -mupdated hello world and for_loops on Dev

--- Hello_world.py
+++ Hello_world.py
@@ -1,1 +1,2 @@
 print ("Hello world ")
+print ("created new branch and checkout ")
--- for_loops.py
+++ for_loops.py
@@ -1,4 +1,5 @@
 for i in range(10):
 	print ("prints 0 to 9")
 	print ("working dir ---> git add ---> staging area ---> git commit ----> local repo(head)")
-	prints ("git rm --cached ----> helps to unstage files from staging area")
+	print ("git rm --cached ----> helps to unstage files from staging area")
+	
