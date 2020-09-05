7096109f356c13d61a26802299490360ff7065dd
Aditya Gautam <adityagautam2011@gmail.com>
2020-09-05 11:01:56 +0530

    -mupdated for_loops.py

--- for_loops.py
+++ for_loops.py
@@ -2,4 +2,5 @@ for i in range(10):
 	print ("prints 0 to 9")
 	print ("working dir ---> git add ---> staging area ---> git commit ----> local repo(head)")
 	print ("git rm --cached ----> helps to unstage files from staging area")
+	print ("git merge command used")
 	
